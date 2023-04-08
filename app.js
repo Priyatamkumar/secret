55f31be0a7d0cd21d12befc4a56724a221ba52c2
priyatam kumar <kpriyatam420@gmail.com>
2023-04-08 14:29:39 +0530

    4th level authentication

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt =require("mongoose-encryption");
+// const encrypt =require("mongoose-encryption");
+// const md5=require("md5");
+const bcrypt= require("bcrypt");
+const saltRounds=10;
+ 
 const app=express();
 
 console.log(process.env.API_KEY);
@@ -22,7 +26,7 @@ const userSchema= new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const user=new mongoose.model("user",userSchema);
 
@@ -38,16 +42,18 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-    const newUser=new user({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save()
-    .then(function () {
-      res.render("secrets");
-    })
-    .catch(function (err) {
-      console.log(err);
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser=new user({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save()
+        .then(function () {
+          res.render("secrets");
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     });
 })
 
@@ -56,9 +62,12 @@ app.post("/login", function(req,res){
     const password=req.body.password;
     user.findOne({email:username})
     .then(function (foundUser) {
-        if(foundUser.password===password){
-            res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result===true)
+            {
+                res.render("secrets");
+            }
+        });
       })
       .catch(function (err) {
         console.log(err);
